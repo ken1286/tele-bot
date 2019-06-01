1b2c4744a725226061f4cf7c1a9962307979090c
ken1286 <kindle1286@gmail.com>
2019-05-31 20:35:44 -0700

    port

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
+const express = require('express');
+const app = express();
 const TelegramBot = require('node-telegram-bot-api');
 const token = "847053296:AAH9MP8G6vw7IW6r5jg6HAggcvllRM9I1i8";
 const bot = new TelegramBot(token, {polling: true});
 const mtg = require('mtgsdk');
 const chatId = "-341485739";
+
+const port = process.env.PORT || 8080
+
+app.listen(port, function() {
+  console.log('Our app is running on http://localhost:' + port);
+});
 
 bot.onText(/\/echo (.+)/, function(msg, match){
   console.log(msg)
@@ -45,3 +53,4 @@ bot.onText(/\/mtg (.+)/, function(msg, match){
       bot.sendMessage(chatId, 'No such card found')
     })
 });
+
